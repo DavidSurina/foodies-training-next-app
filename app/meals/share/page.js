cca1a11c2d3a1d89189d40e42e2a6937a419cc96
daveS <david.surina22@gmail.com>
2024-09-06 12:30:19 +0200

    created MealsFormSubmit btn and placed to the corresponding page

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,4 @@
-"use client";
-
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 import ImagePicker from "@/components/meals/image-picker";
 import { shareMeal } from "@/lib/actions";
 import classes from "./page.module.css";
@@ -44,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
